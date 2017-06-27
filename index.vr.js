7b9da1d6100086f550565f798e464b5fb5293cb9
Michael Cleary <mcleary03@gmail.com>
2017-06-27 13:36:53 -0700

    create custom Shape component to render according to logic in index.js

---
+++ index.vr.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { View, Text, AppRegistry, StyleSheet } from 'react-vr'
+
+import Shape from './vr/components/Shape'
+
+class ShapeGame extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      gameShapes: [1, 1, 1, 1]
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>Find the Odd Shape!</Text>
+        <Shape shapeNum={2} transform={ [ { translate: [0, 0, -5] } ] } />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 0.5,
+    textAlign: 'center',
+    color: '#fff',
+    transform: [
+      {translate: [0, 2, -5]}
+    ]
+  }
+})
+
+AppRegistry.registerComponent('ShapeGame', () => ShapeGame)
