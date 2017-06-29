7ac277c92e0fd2bf120fb6cdc008df43a5a0ea70
Michael Cleary <mcleary03@gmail.com>
2017-06-28 17:33:57 -0700

    add logic for choosing shape, add state to app to remember player score

--- vr/components/Shape.js
+++ vr/components/Shape.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-import {Box, Sphere, Cylinder } from 'react-vr'
+import {Box, Sphere, Cylinder, AsyncStorage } from 'react-vr'
 
 const shapes = [Box, Sphere, Cylinder]
+
+export { shapes }
 
 export default class Shape extends Component {
   render() {
     let Component = shapes[this.props.shapeNum]
+    let colors = ['#c33', '#3c3', '#33c', '#ccc']
 
     return (
       <Component
         style={{
-          color: '#fff',
+          color: colors[this.props.colorNum],
           transform: this.props.transform
         }}
       />
